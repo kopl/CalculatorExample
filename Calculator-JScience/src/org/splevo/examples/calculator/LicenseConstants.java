118384d764f74c501e41ab4b76bce506e5fff25c
Stephan Seifermann <seiferma@users.noreply.github.com>
2015-08-26 12:32:37 +0200

    SPLEVO-460 Initial commit. Extended calculator by CAS license handling functionality.

---
+++ Calculator-JScience/src/org/splevo/examples/calculator/LicenseConstants.java
@@ -0,0 +1,13 @@
+package org.splevo.examples.calculator;
+
+/**
+ * Constants for licensing in the Calculator example.
+ */
+public class LicenseConstants {
+
+	/**
+	 * License module for basic features.
+	 */
+	public static final String BASIC_FEATURES = "BasicFeatures";
+
+}
